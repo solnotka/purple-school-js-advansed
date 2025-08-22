76f7c9c0f20ce0df5483b416ab62c7e73995d183
ngoncharova <ngoncharova@web3tech.ru>
2025-08-22 17:49:30 +0300

    3-birthday

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 "use strict";
 
-const arrFromObj = [
-{ id: 1, name: 'Вася' },
-{ id: 2, name: 'Петя' },
-{ id: 1, name: 'Вася' },
-]
+function checkAge(birthday) {
+  const userBirthday = new Date(birthday);
+  const now = new Date();
 
-const idSet = new Set(arrFromObj.map((item) => item.id));
+  let assumedAge = now.getFullYear() - userBirthday.getFullYear();
 
-const res = [...idSet].map((setId) => arrFromObj.find((item) => item.id === setId))
+  if (userBirthday.getMonth() > now.getMonth() 
+    || (userBirthday.getMonth() === now.getMonth() && userBirthday.getDate() > now.getDate())) {
+    assumedAge -= 1;
+  }
+
+  return assumedAge >= 14 ? true : false;
+}
+
+
+console.log("18 ноября 1986: ", checkAge("1986-11-18"));
+console.log("08 августа 2018: ", checkAge("2018-08-08"));
+console.log("08 августа 2025: ", checkAge("2025-08-08"));
+console.log("23 августа 2024: ", checkAge("2024-08-23"));
+console.log("22 августа 2011: ", checkAge("2011-08-21"));

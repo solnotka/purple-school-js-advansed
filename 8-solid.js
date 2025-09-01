2cf0d49a61d8afc1e32a6bf55ccfcc75ba912fb3
ngoncharova <ngoncharova@web3tech.ru>
2025-09-01 17:36:43 +0300

    fix

--- 8-solid.js
+++ 8-solid.js
@@ -2,9 +2,10 @@ class Billing {
   #amount;
   constructor(amount) {
     this.#amount = amount;
+    this.factor = 1;
   }
   calculateTotal() {
-    return this.#amount;
+    return this.#amount * this.factor;
   }
 }
 
@@ -15,25 +16,18 @@ class FixBilling extends Billing {
 }
 
 class HourBilling extends Billing {
-  hours;
   constructor(amount, hours) {
     super(amount);
     this.hours = hours;
-  }
-  calculateTotal(hours) {
-    return super.calculateTotal() * this.hours;
+    this.factor = this.hours
   }
 }
 
 class ItemBilling extends Billing {
-  items;
   constructor(amount, items) {
     super(amount);
     this.items = items;
     this.factor = this.items;
-  }
-  calculateTotal(items) {
-    return super.calculateTotal() * this.items;
   }
 }
 
